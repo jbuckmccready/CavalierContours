5c9ff1e425c06b69d12add030952f06b038061ba
Buck McCready <jbuckmccready@gmail.com>
2020-03-15 15:10:13 -0700

    Made hilbert index function public in staticspatialindex.h
    - Renamed hilbert function to hilbertXYToIndex
    - To be used for visualization

--- include/cavc/staticspatialindex.h
+++ include/cavc/staticspatialindex.h
@@ -82,7 +82,7 @@ public:
       std::uint32_t hx = static_cast<std::uint32_t>(x);
       Real y = std::floor(hilbertMax * ((minY + maxY) / 2 - m_minY) / height);
       std::uint32_t hy = static_cast<std::uint32_t>(y);
-      hilbertValues[i] = hilbert(hx, hy);
+      hilbertValues[i] = hilbertXYToIndex(hx, hy);
     }
 
     // sort items by their Hilbert value (for packing later)
@@ -194,7 +194,6 @@ public:
       // search through child nodes
       for (std::size_t pos = nodeIndex; pos < end; pos += 4) {
         auto index = m_indices[pos >> 2];
-
         // check if node bbox intersects with query bbox
         if (maxX < m_boxes[pos])
           continue; // maxX < nodeMinX
@@ -226,6 +225,61 @@ public:
         done = true;
       }
     }
+  }
+
+  static std::uint32_t hilbertXYToIndex(std::uint32_t x, std::uint32_t y) {
+    std::uint32_t a = x ^ y;
+    std::uint32_t b = 0xFFFF ^ a;
+    std::uint32_t c = 0xFFFF ^ (x | y);
+    std::uint32_t d = x & (y ^ 0xFFFF);
+
+    std::uint32_t A = a | (b >> 1);
+    std::uint32_t B = (a >> 1) ^ a;
+    std::uint32_t C = ((c >> 1) ^ (b & (d >> 1))) ^ c;
+    std::uint32_t D = ((a & (c >> 1)) ^ (d >> 1)) ^ d;
+
+    a = A;
+    b = B;
+    c = C;
+    d = D;
+    A = ((a & (a >> 2)) ^ (b & (b >> 2)));
+    B = ((a & (b >> 2)) ^ (b & ((a ^ b) >> 2)));
+    C ^= ((a & (c >> 2)) ^ (b & (d >> 2)));
+    D ^= ((b & (c >> 2)) ^ ((a ^ b) & (d >> 2)));
+
+    a = A;
+    b = B;
+    c = C;
+    d = D;
+    A = ((a & (a >> 4)) ^ (b & (b >> 4)));
+    B = ((a & (b >> 4)) ^ (b & ((a ^ b) >> 4)));
+    C ^= ((a & (c >> 4)) ^ (b & (d >> 4)));
+    D ^= ((b & (c >> 4)) ^ ((a ^ b) & (d >> 4)));
+
+    a = A;
+    b = B;
+    c = C;
+    d = D;
+    C ^= ((a & (c >> 8)) ^ (b & (d >> 8)));
+    D ^= ((b & (c >> 8)) ^ ((a ^ b) & (d >> 8)));
+
+    a = C ^ (C >> 1);
+    b = D ^ (D >> 1);
+
+    std::uint32_t i0 = x ^ y;
+    std::uint32_t i1 = b | (0xFFFF ^ (i0 | a));
+
+    i0 = (i0 | (i0 << 8)) & 0x00FF00FF;
+    i0 = (i0 | (i0 << 4)) & 0x0F0F0F0F;
+    i0 = (i0 | (i0 << 2)) & 0x33333333;
+    i0 = (i0 | (i0 << 1)) & 0x55555555;
+
+    i1 = (i1 | (i1 << 8)) & 0x00FF00FF;
+    i1 = (i1 | (i1 << 4)) & 0x0F0F0F0F;
+    i1 = (i1 | (i1 << 2)) & 0x33333333;
+    i1 = (i1 | (i1 << 1)) & 0x55555555;
+
+    return (i1 << 1) | i0;
   }
 
 private:
@@ -290,61 +344,6 @@ private:
     auto e = indices[i];
     indices[i] = indices[j];
     indices[j] = e;
-  }
-
-  static std::uint32_t hilbert(std::uint32_t x, std::uint32_t y) {
-    std::uint32_t a = x ^ y;
-    std::uint32_t b = 0xFFFF ^ a;
-    std::uint32_t c = 0xFFFF ^ (x | y);
-    std::uint32_t d = x & (y ^ 0xFFFF);
-
-    std::uint32_t A = a | (b >> 1);
-    std::uint32_t B = (a >> 1) ^ a;
-    std::uint32_t C = ((c >> 1) ^ (b & (d >> 1))) ^ c;
-    std::uint32_t D = ((a & (c >> 1)) ^ (d >> 1)) ^ d;
-
-    a = A;
-    b = B;
-    c = C;
-    d = D;
-    A = ((a & (a >> 2)) ^ (b & (b >> 2)));
-    B = ((a & (b >> 2)) ^ (b & ((a ^ b) >> 2)));
-    C ^= ((a & (c >> 2)) ^ (b & (d >> 2)));
-    D ^= ((b & (c >> 2)) ^ ((a ^ b) & (d >> 2)));
-
-    a = A;
-    b = B;
-    c = C;
-    d = D;
-    A = ((a & (a >> 4)) ^ (b & (b >> 4)));
-    B = ((a & (b >> 4)) ^ (b & ((a ^ b) >> 4)));
-    C ^= ((a & (c >> 4)) ^ (b & (d >> 4)));
-    D ^= ((b & (c >> 4)) ^ ((a ^ b) & (d >> 4)));
-
-    a = A;
-    b = B;
-    c = C;
-    d = D;
-    C ^= ((a & (c >> 8)) ^ (b & (d >> 8)));
-    D ^= ((b & (c >> 8)) ^ ((a ^ b) & (d >> 8)));
-
-    a = C ^ (C >> 1);
-    b = D ^ (D >> 1);
-
-    std::uint32_t i0 = x ^ y;
-    std::uint32_t i1 = b | (0xFFFF ^ (i0 | a));
-
-    i0 = (i0 | (i0 << 8)) & 0x00FF00FF;
-    i0 = (i0 | (i0 << 4)) & 0x0F0F0F0F;
-    i0 = (i0 | (i0 << 2)) & 0x33333333;
-    i0 = (i0 | (i0 << 1)) & 0x55555555;
-
-    i1 = (i1 | (i1 << 8)) & 0x00FF00FF;
-    i1 = (i1 | (i1 << 4)) & 0x0F0F0F0F;
-    i1 = (i1 | (i1 << 2)) & 0x33333333;
-    i1 = (i1 | (i1 << 1)) & 0x55555555;
-
-    return (i1 << 1) | i0;
   }
 };
 } // namespace cavc
