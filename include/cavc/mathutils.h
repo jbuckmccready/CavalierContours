677c69816d993e5a1ee8fd68ecb0f81b419c4d1f
Buck McCready <jbuckmccready@gmail.com>
2020-04-05 13:41:15 -0700

    Replaced placeholder index pair hash
    - Eliminates some collisions for a slight performance improvement

--- include/cavc/mathutils.h
+++ include/cavc/mathutils.h
@@ -110,9 +110,18 @@ template <typename T> std::size_t prevWrappingIndex(std::size_t index, const T &
   return index - 1;
 }
 
+template <typename T> inline void hashCombine(std::size_t &seed, const T &val) {
+  // copied from boost hash_combine, it's not the best hash combine but it's very simple
+  // https://stackoverflow.com/questions/35985960/c-why-is-boosthash-combine-the-best-way-to-combine-hash-values
+  seed ^= std::hash<T>()(val) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
 struct IndexPairHash {
-  std::size_t operator()(std::pair<std::size_t, std::size_t> const &pair) const {
-    return std::hash<std::size_t>()(pair.first) ^ std::hash<std::size_t>()(pair.second);
+  inline std::size_t operator()(std::pair<std::size_t, std::size_t> const &pair) const {
+    std::size_t seed = 0;
+    hashCombine(seed, pair.first);
+    hashCombine(seed, pair.second);
+    return seed;
   }
 };
 
